256dab364f9705db07f16e873d1388464c777323
fifa2002nb <xysmiracle@hotmail.com>
2015-08-20 15:21:50 +0800

    update

--- dockerfiles/install.sh
+++ dockerfiles/install.sh
@@ -5,7 +5,9 @@ PHP_DIR=${ROOT_DIR}/php
 JSSPEED_DIR=${ROOT_DIR}/jsspeed
 NGINX_DIR=${ROOT_DIR}/nginx
 DATA_DIR=${ROOT_DIR}/data
+SOURCE_DIR=../
 
 `mkdir -p ${PHP_DIR}/log && mkdir -p ${PHP_DIR}/var && mkdir -p ${JSSPEED_DIR} && mkdir -p ${NGINX_DIR}/log && mkdir -p ${NGINX_DIR}/www && mkdir -p ${DATA_DIR}`
+`cp -r ${SOURCE_DIR}/miles ${NGINX_DIR}/www/`
 
 echo "execute => 'docker-compose up'"
