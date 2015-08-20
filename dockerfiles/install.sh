40bccb18027a8e9fb0d5e83d9ead68d0c663740a
fifa2002nb <xysmiracle@hotmail.com>
2015-08-20 13:38:11 +0800

    update

--- dockerfiles/install.sh
+++ dockerfiles/install.sh
@@ -6,3 +6,4 @@ JSSPEED_DIR=${ROOT_DIR}/jsspeed
 NGINX_DIR=${ROOT_DIR}/nginx
 
 `mkdir -p ${PHP_DIR}/log && mkdir -p ${PHP_DIR}/var && mkdir -P ${JSSPEED_DIR} && mkdir -P ${NGINX_DIR}/log`
+`docker-compose up`
