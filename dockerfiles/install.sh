6d33b681379e74420c2a18f49f9f25525eafe077
fifa2002nb <xysmiracle@hotmail.com>
2015-08-20 15:24:33 +0800

    update

--- dockerfiles/install.sh
+++ dockerfiles/install.sh
@@ -9,5 +9,6 @@ SOURCE_DIR=../
 
 `mkdir -p ${PHP_DIR}/log && mkdir -p ${PHP_DIR}/var && mkdir -p ${JSSPEED_DIR} && mkdir -p ${NGINX_DIR}/log && mkdir -p ${NGINX_DIR}/www && mkdir -p ${DATA_DIR}`
 `cp -r ${SOURCE_DIR}/miles ${NGINX_DIR}/www/`
+`cp -r ${SOURCE_DIR}/phpmyadmin ${NGINX_DIR}/www/`
 
 echo "execute => 'docker-compose up'"
